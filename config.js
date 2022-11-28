844bfb4b1b08dd2358745a8d63eaf0245d8348a8
Arnav Maurya <99680938+ArnavM3108@users.noreply.github.com>
2022-11-28 18:45:29 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+//initialize your database
+const firebaseConfig = {
+  apiKey: "AIzaSyB7WHl4jxh85fiwcY4Jy5mZsqb0BSTmcQk",
+  authDomain: "school-attendance-edd2d.firebaseapp.com",
+  databaseURL: "https://school-attendance-edd2d-default-rtdb.firebaseio.com",
+  projectId: "school-attendance-edd2d",
+  storageBucket: "school-attendance-edd2d.appspot.com",
+  messagingSenderId: "834524306610",
+  appId: "1:834524306610:web:8c93736e66a47464bf3dab"
+};
+
+
+  export default firebase.database()
+ 
+
+  
